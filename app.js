43326fc44828d5489b4e6354aab55e2ff579c187
Karunakar HV <frontendacadglid@gmail.com>
2016-11-03 20:07:43 +0530

    JavaScript Comparison and if-else operators
    Create a JavaScript file app.js and include that script file into your index.html file using <script> tag. In app.js file, write a JavaScript program to find the greatest among 3 numbers and display the greatest number in console.

---
+++ app.js
@@ -0,0 +1,16 @@
+var num1 = 2;
+var num2 = 3;
+var num3 = 1;
+//Print Greatest of 3 numbers
+if(num1 > num2)
+{
+  if(num1 > num3)
+    console.log("Greatest Number is: " + num1);
+}
+else if(num2 > num3)
+{
+  console.log("Greatest Number is: " + num2);
+}
+else {
+  console.log("Greatest Number is: " + num3);
+}
